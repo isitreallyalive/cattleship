847c981ba49dc2f6d499203af08fcfeeb72ac360
newt <hi@newty.dev>
2025-09-03 15:22:04 +0100

    feat: select tiles

--- main.c
+++ main.c
@@ -3,80 +3,68 @@
 
 #define SIZE 8
 
-enum tile {
+enum tile
+{
     TILE_EMPTY
 };
 typedef enum tile tile_t;
 
-struct pos {
+struct pos
+{
     int x;
     int y;
 };
 typedef struct pos pos_t;
 
-/**
- * Prints a string to the terminal using the specified color pair.
- *
- * @param string The string to print.
- * @param n The color pair number to use.
- */
-void printwc(const char *string, int n) {
-    attron(COLOR_PAIR(n));
-    printw("%s", string);
-    attroff(COLOR_PAIR(n));
-}
-
-void render(tile_t board[SIZE][SIZE], pos_t selected) {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            char c;
-            int color = 0;
-            if (selected.x == j && selected.y == i) {
-                color = 1;
-            }
-
-            switch (board[i][j]) {
-                case TILE_EMPTY:
-                    c = '-';
-                    break;
-            }
-
-            addch(c | COLOR_PAIR(color));
+void render(tile_t board[SIZE][SIZE], pos_t pos)
+{
+    clear();
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+        {
+            int color = (pos.x == j && pos.y == i) ? 1 : 0;
+            addch('-' | COLOR_PAIR(color));
             addch(' ');
+            if (j == SIZE - 1)
+                move(i + 1, 0);
         }
-        addch('\n');
-    }
+    refresh();
 }
 
-int main() {
-    // --- ncurses ---
+int main()
+{
     // init
     initscr();
     curs_set(0);
-
-    // color support
-    if (has_colors()) {
+    if (has_colors())
+    {
         start_color();
         init_pair(1, COLOR_BLACK, COLOR_WHITE);
     }
+    keypad(stdscr, TRUE);
 
-    // --- game ---
-    // populate board
-    tile_t board[SIZE][SIZE];
-    pos_t selected;
-    selected.x = 0;
-    selected.y = 0;
-    
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            board[i][j] = TILE_EMPTY;
-        }
+    tile_t board[SIZE][SIZE] = {0};
+    pos_t pos = {0, 0};
+
+    // game loop
+    int ch;
+    while (1)
+    {
+        render(board, pos);
+        ch = getch();
+        if (ch == 'q')
+            break;
+        if (ch == KEY_UP && pos.y > 0)
+            pos.y--;
+        if (ch == KEY_DOWN && pos.y < SIZE - 1)
+            pos.y++;
+        if (ch == KEY_LEFT && pos.x > 0)
+            pos.x--;
+        if (ch == KEY_RIGHT && pos.x < SIZE - 1)
+            pos.x++;
     }
 
-    render(board, selected);
-    getch();
-
-    // cleanup
+    // teardown
     endwin();
     return 0;
 }
